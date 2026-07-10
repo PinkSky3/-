186fde26b918223999204c1552a5c1bc2ffe3635
User <user@example.com>
2026-07-10 16:24:48 +0800

    Release version 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "com.dailyhot.android"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.2.2"
+    versionCode = 4
+    versionName = "1.3.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
